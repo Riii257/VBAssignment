1a20025ef0cededb86f55d1ae89fc4a808f00363
Riii257 <115649544+Riii257@users.noreply.github.com>
2024-04-06 14:01:27 +0530

    Update Program.vb

--- assignment/Program.vb
+++ assignment/Program.vb
@@ -1,15 +1,18 @@
 Imports System.Console
 
 Module Program
+    ' Create class
     Public Class Student
         Public S_ID, Marks1, Marks2 As Integer
         Public Name As String
+        ' Create constructor
         Public Sub GetDetails(ByVal id As Integer, ByVal nm As String, ByVal mks1 As Integer, ByVal mks2 As Integer)
             S_ID = id
             Name = nm
             Marks1 = mks1
             Marks2 = mks2
         End Sub
+        ' Create functions
         Public Function Display()
             WriteLine("Student ID: " & S_ID)
             WriteLine("Student Name: " & Name)
@@ -21,6 +24,9 @@ Module Program
         End Function
     End Class
     Sub Main()
+
+
+       ' Create object
         Dim St1 As Student = New Student()
         Dim id, mks1, mks2 As Integer
         Dim nm As String
@@ -37,8 +43,11 @@ Module Program
         Write("Enter marks 2: ")
         mks2 = ReadLine()
 
+        ' Calling Constructir
         St1.GetDetails(id, nm, mks1, mks2)
 
+
+        ' Calling functions
         St1.Display()
 
         WriteLine("Sum of marks= " & St1.Total())
